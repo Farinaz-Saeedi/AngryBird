89c972cd791726b6e85d12b25f688a49cc76ad4c
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-13 18:15:18 +0330

    [FEAT]: override readInputs function in Scenario2 class

--- src/Scenario2.cpp
+++ src/Scenario2.cpp
@@ -2,5 +2,56 @@
 
 void Scenario::readInputs(std::string fileName)
 {
+    std::ifstream input("Scenario2.txt");
+    if (!input.is_open())
+        std::cerr << " Unable to open file ! \n";
+
+    ll count, number;
+    std::string str;
+    Type bType;
+    Base base;
+
+    while (!input.eof())
+    {
+        input >> count;
+        for (int i = 0; i < count; i++)
+        {
+            input >> str;
+            birds[i].setName(str);
+
+            input >> number;
+            birds[i].setDistance(number);
+
+            input >> number;
+            birds[i].setOutOfControl(number);
+
+            input >> number;
+            birds[i].setDegree(number);
+
+            input >> number;
+            birds[i].setDemolition(number);
+
+            input >> str;
+            bType = A;
+            birds[i].setType(bType);
+
+            input >> str;
+            if (str.find('N'))
+            {
+                base = N;
+            }
+            else if (str.find('E'))
+            {
+                base = E;
+            }
+            else if (str.find('H'))
+            {
+                base = H;
+            }
+
+            birds[i].setBase(base);
+        }
+    }
     
+    input.close();
 }
