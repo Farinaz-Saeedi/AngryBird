cc04456d2f784b206aabba9f1574762a53542e04
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-13 18:38:30 +0330

    [FEAT]: override readInputs function in Scenario4 class

--- src/Scenario4.cpp
+++ src/Scenario4.cpp
@@ -2,5 +2,68 @@
 
 void Scenario4::readInputs(std::string fileName)
 {
+    std::ifstream input("Scenario4.txt");
+    if (!input.is_open())
+        std::cerr << " Unable to open file ! \n";
 
+    ll count, number;
+    std::string str;
+    Type bType;
+    Base base;
+
+    while (!input.eof())
+    {
+        input >> count;
+        for (int i = 0; i < count; i++)
+        {
+            input >> str;
+            getBirds()[i].setName(str);
+
+            input >> number;
+            getBirds()[i].setDistance(number);
+
+            input >> number;
+            getBirds()[i].setOutOfControl(number);
+
+            input >> number;
+            getBirds()[i].setDegree(number);
+
+            input >> number;
+            getBirds()[i].setDemolition(number);
+
+            input >> str;
+            if (str.find('A'))
+            {
+                bType = A;
+            }
+            else if (str.find('B'))
+            {
+                bType = B;
+            }
+            else if (str.find('C'))
+            {
+                bType = C;
+            }
+            
+            getBirds()[i].setType(bType);
+
+            input >> str;
+            if (str.find('N'))
+            {
+                base = N;
+            }
+            else if (str.find('E'))
+            {
+                base = E;
+            }
+            else if (str.find('H'))
+            {
+                base = H;
+            }
+
+            getBirds()[i].setBase(base);
+        }
+    }
+
+    input.close();
 }
