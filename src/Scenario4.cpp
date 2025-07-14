bd211cf7b4439eebd40f981514faf6cae10107d6
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-15 00:49:41 +0330

    [FIX]: fix reading input in Scenario-4

--- src/Scenario4.cpp
+++ src/Scenario4.cpp
@@ -1,50 +1,26 @@
 #include "Scenario4.hpp"
 
-void Scenario4::readInputs(std::vector<Bird> & birds)
+void Scenario4::readInputs(std::vector<Bird> &birds, std::vector<Home> &homes)
 {
     std::ifstream input("Scenario4.txt");
     if (!input.is_open())
         std::cerr << " Unable to open file ! \n";
 
-    ll count, number;
-    std::string str;
-    Type bType;
+    ll count;
+    std::string name;
+    int cp;
 
     while (!input.eof())
     {
         input >> count;
         for (int i = 0; i < count; i++)
         {
-            input >> str;
-            getBirds()[i].setName(str);
+           input >> name;
+           readBird(name , birds);
 
-            input >> number;
-            getBirds()[i].setDistance(number);
-
-            input >> number;
-            getBirds()[i].setOutOfControl(number);
-
-            input >> number;
-            getBirds()[i].setDegree(number);
-
-            input >> number;
-            getBirds()[i].setDemolition(number);
-
-            input >> str;
-            if (str.find('A'))
-            {
-                bType = A;
-            }
-            else if (str.find('B'))
-            {
-                bType = B;
-            }
-            else if (str.find('C'))
-            {
-                bType = C;
-            }
-            
-            getBirds()[i].setType(bType);
+           input >> name;
+           input >> cp;
+           readHome(name, cp, homes);
         }
     }
 
