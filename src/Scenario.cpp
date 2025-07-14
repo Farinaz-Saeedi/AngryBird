0bb72c1d69b09590de00dea56ce45d554d16e8b5
Sahba-M <sahba.mir.2005@gmail.com>
2025-07-14 15:58:00 +0330

    [REFACTOR]: write readBirds function in Scenario class

--- src/Scenario.cpp
+++ src/Scenario.cpp
@@ -2,6 +2,30 @@
 
 #include "Scenario.hpp"
 
+void Scenario::readBird(std::string targetName , std::vector<Bird> & birds)
+{
+    std::ifstream input("birds.txt");
+    if (!input.is_open())
+        std::cerr << " Unable to open file ! \n";
+
+    std::string line;
+
+    while (std::getline(input, line)) 
+    {
+        std::string name , temp ;
+        ll dist , out , demol ; 
+        int degree , count ;
+        Type t;
+        std::istringstream iss(line);
+        if (iss >> name >> dist >> out >> degree >> demol >> t) {
+            if (name == targetName) {
+                input.close();
+                birds.push_back(Bird(name , dist , out , degree , demol , t));
+            }
+        }
+    }
+}
+
 // std::vector<Bird> Scenario::getBirds()
 // {
 //     return birds;
