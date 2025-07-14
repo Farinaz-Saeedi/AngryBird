570fdc2a58ba0c17ce2f4c600d66f8f77e214c40
Sahba-M <sahba.mir.2005@gmail.com>
2025-07-14 16:00:00 +0330

    [FIX]: fix reading input Scenario-1

--- src/Scenario1.cpp
+++ src/Scenario1.cpp
@@ -6,33 +6,16 @@ void Scenario1::readInputs(std::vector<Bird> & birds)
     if (!input.is_open())
         std::cerr << " Unable to open file ! \n";
 
-    ll count, number;
-    std::string str;
-    Type bType;
+    std::string name ;
+    int count ;
 
     while (!input.eof())
     {
         input >> count;
         for (int i = 0; i < count; i++)
         {
-            input >> str;
-            getBirds()[i].setName(str);
-
-            input >> number;
-            getBirds()[i].setDistance(number);
-
-            input >> number;
-            getBirds()[i].setOutOfControl(number);
-
-            input >> number;
-            getBirds()[i].setDegree(number);
-
-            input >> number;
-            getBirds()[i].setDemolition(number);
-
-            input >> str;
-            bType = D;
-            getBirds()[i].setType(bType);
+            input >> name;
+            readBird(name , birds);
         }
     }
 
