56272e44015f98a2d316602debeccef464b560f2
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-17 14:07:47 +0330

    [FEAT]: add setter and getter for nights

--- src/Scenario7.cpp
+++ src/Scenario7.cpp
@@ -1,1 +1,10 @@
 #include "Scenario5.hpp"
+
+int Scenario5::getNumberOfNights()
+{
+    return numberOfNights;
+}
+void Scenario5::setNumberOfNights(int numberOfNights)
+{
+    this->numberOfNights = numberOfNights;
+}
