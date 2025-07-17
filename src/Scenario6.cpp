20dc1abfa29c59bf9cf639eb8f742d4441e60858
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-17 14:02:04 +0330

    [FEAT]: override readInputs function

--- src/Scenario6.cpp
+++ src/Scenario6.cpp
@@ -1,1 +1,28 @@
 #include "Scenario6.hpp"
+
+void Scenario6::readInputs(std::vector<Bird> &birds, std::vector<Home> &homes)
+{
+    std::ifstream input("Scenario6.txt");
+    if (!input.is_open())
+        std::cerr << " Unable to open file ! \n";
+
+    ll count;
+    std::string name;
+    int cp;
+
+    while (!input.eof())
+    {
+        input >> count;
+        for (int i = 0; i < count; i++)
+        {
+           input >> name;
+           readBird(name , birds);
+
+           input >> name;
+           input >> cp;
+           readHome(name, cp, homes);
+        }
+    }
+
+    input.close();
+}
