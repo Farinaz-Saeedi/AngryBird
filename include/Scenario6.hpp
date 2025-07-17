a83b656abef212b2e49aaafda62ead5e3c670c44
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-17 13:59:41 +0330

    [FEAT]: make a new class for Scenario6

---
+++ include/Scenario6.hpp
@@ -0,0 +1,18 @@
+#ifndef SCENARIO6_H
+#define SCENARIO6_H
+
+#include "Scenario.hpp"
+
+class Scenario6 : public Scenario
+{
+    public:
+        void readInputs(std::vector<Bird> & birds, std::vector<Home> & homes) override;
+       
+
+    private:
+       
+
+};
+
+
+#endif 
