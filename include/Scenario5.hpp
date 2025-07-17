4f18d5432044ce110804e7edf31d231850c9b557
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-17 13:41:43 +0330

    [FEAX]: make a new class for Scenario5

---
+++ include/Scenario5.hpp
@@ -0,0 +1,17 @@
+#ifndef SCENARIO5_H
+#define SCENARIO5_H
+
+#include "Scenario.hpp"
+
+class Scenario5 : public Scenario
+{
+    public:
+        void readInputs(std::vector<Bird> & birds, std::vector<Home> & homes) override;
+
+
+    private:
+
+};
+
+
+#endif 
