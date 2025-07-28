ad8e8378edb04ed9bb71ec10d2c658d1411c112b
Farinaz-Saeedi <170177169+Farinaz-Saeedi@users.noreply.github.com>
2025-07-29 00:34:34 +0330

    [REFACTOR]: some changes in readInputs function in Scenario4 , Scenario5

--- src/Scenario4.cpp
+++ src/Scenario4.cpp
@@ -14,11 +14,20 @@ void Scenario4::readInputs(std::vector<Bird> &birds, std::vector<Home> &homes)
         input >> count;
         for (int i = 0; i < count; i++)
         {
-           input >> name;
-           readBird(name , birds);
+            input >> name;
+            Bird temp = readBird(name, birds);
 
-           input >> name;
-           birds[birds.size()-1].setHomePlace(name);
+            input >> name;
+            birds[birds.size() - 1].setHomePlace(name);
+
+            for (int j = 0; j < homes.size(); j++)
+            {
+                if (homes[j].getCityName() == name)
+                {
+                    homes[j].push(temp);
+                    break;
+                }
+            }
         }
     }
 
--- src/Scenario5.cpp
+++ src/Scenario5.cpp
@@ -23,18 +23,27 @@ void Scenario5::readInputs(std::vector<Bird> &birds, std::vector<Home> &homes)
     {
         input >> nights;
         setNumberOfNights(nights);
-        
+
         input >> count;
         for (int i = 0; i < count; i++)
         {
-           input >> name;
-           readBird(name , birds);
+            input >> name;
+            Bird temp = readBird(name, birds);
 
-           input >> name;
-           birds[birds.size()-1].setHomePlace(name);
+            input >> name;
+            birds[birds.size() - 1].setHomePlace(name);
+
+            for (int j = 0; j < homes.size(); j++)
+            {
+                if (homes[j].getCityName() == name)
+                {
+                    homes[j].push(temp);
+                    break;
+                }
+            }
         }
     }
 
     input.close();
 }
-void Scenario5::printOutput(Controler &control){}
+void Scenario5::printOutput(Controler &control) {}
